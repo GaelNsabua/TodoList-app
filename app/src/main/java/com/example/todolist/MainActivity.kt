7712e401589a607d9f3e26339d3dc853a0432e57
Gael Mukala <gaelmukala14@gmail.com>
2025-02-14 15:37:57 +0100

    Fisrt commit

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.todolist
+
+
+import NavigationGraph
+import TaskScreen
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todolist.ui.theme.TodolistTheme
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationGraph()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskPreview() {
+
+}
